b445cd2d8196d0a4ebfeffdb36b00f0d932154e5
ludvij <63416856+ludvij@users.noreply.github.com>
2022-02-11 13:06:51 +0100

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -9,7 +9,6 @@ for (( i=0; i < ${#HOSTNAME}; i++ )); do
 	host_name="${host_name}\[\e[1;38;5;${colorchar}m\]${HOSTNAME:$i:1}"
 done
 purple="\[\e[38;5;93m\]"
-blue="\[\e[38;5;111m\]"
 normal="\[\e[m\]"
 
 export PS1="${purple}[${user_name}${purple}@${normal}${host_name}${purple}]${normal} \w${purple}\n> ${normal}"
